4d8eb047f5ff776088ec82a374d3f05d97f723c0
Reavar14 <faizulahmad09@gmail.com>
2026-06-17 02:05:44 +0700

    fix: update axios baseURL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://catatankeuangan-production-6814.up.railway.app/api',
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
